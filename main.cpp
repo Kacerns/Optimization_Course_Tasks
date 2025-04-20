c43b0e99472ef79e7ec87aa898decf78ab364c80
karocern <karoliscernevicius@gmail.com>
2025-04-20 16:24:07 +0300

    feat(TaskSetUp):Made objective function and equality, inequality constraints

--- main.cpp
+++ main.cpp
@@ -4,33 +4,29 @@
 
 using namespace std;
 
-// Tikslo funkcija: minimizuojame -x*y*z (maksimizuojame tūrį)
 double objective_function(const vector<double>& X) {
-    double x = X[0];
-    double y = X[1];
-    double z = X[2];
+    double x = X.at(0);
+    double y = X.at(1);
+    double z = X.at(2);
     return -x * y * z;
 }
 
-// Lygybinio apribojimo funkcija: 2(xy + xz + yz) - 1 = 0
 double equality_constraint(const vector<double>& X) {
-    double x = X[0];
-    double y = X[1];
-    double z = X[2];
+    double x = X.at(0);
+    double y = X.at(1);
+    double z = X.at(2);
     return 2.0 * (x * y + x * z + y * z) - 1.0;
 }
 
-// Nelygybinių apribojimų funkcijos: -x ≤ 0, -y ≤ 0, -z ≤ 0
 vector<double> inequality_constraints(const vector<double>& X) {
-    double x = X[0];
-    double y = X[1];
-    double z = X[2];
+    double x = X.at(0);
+    double y = X.at(1);
+    double z = X.at(2);
     vector<double> h = {-x, -y, -z};
     return h;
 }
 
 int main() {
-    // Pradinis taškas - galima naudoti įvairius optimizavimo algoritmus
     vector<double> initial_point = {0.1, 0.1, 0.1};
     
     cout << "Optimizavimo uždavinys:\n";
